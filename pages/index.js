387c4d2fd454bd203b6bcb3b217bc58b7d31d45e
Pratik Gadhiya <pratik.gadhiya@hotmail.com>
2020-08-01 21:23:22 +0530

    feat: add to home screen

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,46 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
 import Form from '../components/Form';
 
-const App = () => (
-  <>
-    <Head>
-      <title>Whatsapp unsaved contact</title>
-    </Head>
-    <Layout>
+const App = () => {
 
-      <h1 className="display-5">WhatsApp unsaved contact</h1>
-      <p className="lead">No hassle. Send whatsapp to anyone without saving number.</p>
+  const [deferredPrompt, updatedDeferredPrompt] = useState(null);
 
-      <Form />
-    </Layout>
-  </>
-);
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      updatedDeferredPrompt(e);
+    });
+  }, []);
+
+  const onA2hsClickHandler = () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+    });
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Whatsapp unsaved contact</title>
+      </Head>
+      <Layout>
+
+        <h1 className="display-5">WhatsApp unsaved contact</h1>
+        <p className="lead">No hassle. Send whatsapp to anyone without saving number.</p>
+
+        <Form />
+
+        <button className="btn btn-secondary" onClick={onA2hsClickHandler}>Add to home screen</button>
+      </Layout>
+    </>
+  );
+}
 
 export default App;
